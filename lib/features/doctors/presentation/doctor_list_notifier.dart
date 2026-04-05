c6a4868ad324912c002d670f1cd6c16bf5de0dec
fawadahmed96 <fawadahmed017@gmail.com>
2026-04-05 15:50:41 +0500

    add doctor search functionality using StateProvider

--- lib/features/doctors/presentation/doctor_list_notifier.dart
+++ lib/features/doctors/presentation/doctor_list_notifier.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_finder/features/doctors/data/doctor_repository_impl.dart';
 import 'package:doctor_finder/features/doctors/domain/doctor_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final doctorRepositoryProvider = Provider<DoctorRepositoryImpl>(
   (ref) => DoctorRepositoryImpl(),
@@ -9,4 +10,8 @@ final doctorRepositoryProvider = Provider<DoctorRepositoryImpl>(
 final doctorProvider = FutureProvider<List<DoctorModel>>((ref) async {
   final repository = ref.watch(doctorRepositoryProvider);
   return repository.fetchDoctors();
+
 });
+
+final searchQueryProvider=StateProvider<String>((ref)=>'');
+
--- lib/features/doctors/presentation/doctor_list_screen.dart
+++ lib/features/doctors/presentation/doctor_list_screen.dart
@@ -7,48 +7,107 @@ class DoctorListScreen extends ConsumerWidget {
   const DoctorListScreen({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-    final doctorAsync=ref.watch(doctorProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final doctorAsync = ref.watch(doctorProvider);
+    final searchQuery = ref.watch(searchQueryProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Doctors'),
-      ),
+      appBar: AppBar(title: Text('Doctors List')),
       body: doctorAsync.when(
-        loading: ()=>Center(child: CircularProgressIndicator()),
-        error:(error,stack)=>Center(child: Text('Error: $error')),
-        data: (doctors)=>ListView.builder(
-            itemCount: doctors.length,
-            itemBuilder: (context,index) {
-              final doctor=doctors[index];
-              return Card(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(child: Text('Error: $error')),
+        data: (doctors) {
+          final filtered =
+              doctors
+                  .where(
+                    (doctor) =>
+                        doctor.doctorName.toLowerCase().contains(searchQuery.toLowerCase()),
+                  )
+                  .toList();
+
+          return Column(
+              children: [
+                Padding(padding: const EdgeInsets.all(16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search doctors...',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged:(value)=> ref.read(searchQueryProvider.notifier).state=value ,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        doctor.doctorName,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(doctor.specialityEnglish),
-                      const SizedBox(height: 4),
-                      Text(doctor.locationName),
-                      const SizedBox(height: 4),
-                      Text('Rs. ${doctor.charges.initialCharges}'),
-                    ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: filtered.length,
+                      itemBuilder:(context,index){
+                        final doctor=filtered[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  doctor.doctorName,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(doctor.specialityEnglish),
+                                const SizedBox(height: 4),
+                                Text(doctor.locationName),
+                                const SizedBox(height: 4),
+                                Text('Rs. ${doctor.charges.initialCharges}'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
                   ),
                 ),
-              );
+              ]
+          );
+        },
 
-            }),
+        // data: (doctors)=>ListView.builder(
+        //     itemCount: doctors.length,
+        //     itemBuilder: (context,index) {
+        //       final doctor=doctors[index];
+        //       return Card(
+        //         margin: const EdgeInsets.symmetric(
+        //           horizontal: 16,
+        //           vertical: 8,
+        //         ),
+        //         child: Padding(
+        //           padding: const EdgeInsets.all(16),
+        //           child : Column(
+        //             crossAxisAlignment: CrossAxisAlignment.start,
+        //             children: [
+        //               Text(
+        //                 doctor.doctorName,
+        //                 style: const TextStyle(
+        //                   fontSize: 16,
+        //                   fontWeight: FontWeight.bold,
+        //                 ),
+        //               ),
+        //               const SizedBox(height: 4),
+        //               Text(doctor.specialityEnglish),
+        //               const SizedBox(height: 4),
+        //               Text(doctor.locationName),
+        //               const SizedBox(height: 4),
+        //               Text('Rs. ${doctor.charges.initialCharges}'),
+        //             ],
+        //           ),
+        //         ),
+        //       );
+        //
+        //     }),
       ),
     );
   }
